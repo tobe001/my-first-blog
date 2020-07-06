992f1acb9fec5a292b311987e71891bae9d8e0e5
tobe001 <tgb811@student.bham.ac.uk>
2020-07-06 14:54:14 +0100

    Added urls to CV app, along with first passing unit test.

--- cv/tests.py
+++ cv/tests.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from cv.views import cv
 
 # Create your tests here.
 
-class SillyTest(TestCase):
+class CVPageTest(TestCase):
 	
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1, 3)
+	def test_cv_url_resolves_to_correct_view(self):
+		found = resolve("/cv/")
+		self.assertEqual(found.func, cv)
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('cv/', include('cv.urls')),
 	path('', include('blog.urls')),
 ]

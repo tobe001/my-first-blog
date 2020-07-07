30e7dab13cf0e529a794dafc99200dfef7fad247
tobe001 <tgb811@student.bham.ac.uk>
2020-07-07 10:33:14 +0100

    CV view now returns minimal HTML.

--- cv/tests.py
+++ cv/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from cv.views import cv
 
 # Create your tests here.
@@ -9,3 +10,11 @@ class CVPageTest(TestCase):
 	def test_cv_url_resolves_to_correct_view(self):
 		found = resolve("/cv/")
 		self.assertEqual(found.func, cv)
+	
+	def test_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = cv(request)
+		html = response.content.decode("utf8")
+		self.assertTrue(html.startswith("<html>"))
+		self.assertIn("<title>My CV</title>", html)
+		self.assertTrue(html.endswith("</html>"))

74944ffe80e3042c3a8bba8864dd42d942cfcece
tobe001 <tgb811@student.bham.ac.uk>
2020-07-07 13:40:12 +0100

    Changed CV page to use template.

--- cv/tests.py
+++ cv/tests.py
@@ -1,20 +1,9 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from cv.views import cv
 
 # Create your tests here.
 
 class CVPageTest(TestCase):
 	
-	def test_cv_url_resolves_to_correct_view(self):
-		found = resolve("/cv/")
-		self.assertEqual(found.func, cv)
-	
-	def test_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = cv(request)
-		html = response.content.decode("utf8")
-		self.assertTrue(html.startswith("<html>"))
-		self.assertIn("<title>My CV</title>", html)
-		self.assertTrue(html.endswith("</html>"))
+	def test_page_uses_cv_template(self):
+		response = self.client.get("/cv/")
+		self.assertTemplateUsed(response, "cv.html")
--- cv/views.py
+++ cv/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def cv(request):
-	return HttpResponse("<html><title>My CV</title></html>")
+	return render(request, "cv.html")

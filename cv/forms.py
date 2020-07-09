a10df913a8facd3c2dabf9878182392dc463d73c
tobe001 <tgb811@student.bham.ac.uk>
2020-07-09 14:54:16 +0100

    Refactored CV page to use Django form to enter education items

--- cv/forms.py
+++ cv/forms.py
@@ -1,5 +1,12 @@
 from django import forms
-from .models import KeySkill
+from .models import KeySkill, Education
 
 class KeySkillForm(forms.Form):
 	skills_input_text = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter a new skill"}))
+
+class EducationForm(forms.Form):
+	education_input_start_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter start year"}))
+	education_input_end_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter end year"}))
+	education_input_institution = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter institution name"}))
+	education_input_course_title = forms.CharField(max_length = 100, required = False, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter course title"}))
+	education_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details"}))
--- cv/views.py
+++ cv/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from cv.models import KeySkill, Education
-from .forms import KeySkillForm
+from .forms import KeySkillForm, EducationForm
 
 # Create your views here.
 
 def cv(request):
 	if request.method == "POST":
 		new_skill_form = KeySkillForm(request.POST)
+		new_education_form = EducationForm(request.POST)
 		if new_skill_form.is_valid():
 			new_skill = KeySkill()
 			new_skill.text = request.POST["skills_input_text"]
 			new_skill.save()
 			return redirect("/cv/")
-		else:
+		elif new_education_form.is_valid():
 			new_education = Education()
 			new_education.start_year = request.POST["education_input_start_year"]
 			new_education.end_year = request.POST["education_input_end_year"]
@@ -25,5 +26,6 @@ def cv(request):
 	skills = KeySkill.objects.all()
 	skills_form = KeySkillForm()
 	education_items = Education.objects.all().order_by("-start_year")
+	education_form = EducationForm()
 	return render(request, "cv.html", {"skills": skills, "skills_form": skills_form,
-										"education_items": education_items})
+										"education_items": education_items, "education_form": education_form})

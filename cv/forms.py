c1b79a87d172747455ee84cf3add467f07a7aec6
tobe001 <tgb811@student.bham.ac.uk>
2020-07-10 14:23:00 +0100

    Refactored work experience section to use Django form

--- cv/forms.py
+++ cv/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .models import KeySkill, Education
 
 class KeySkillForm(forms.Form):
 	skills_input_text = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter a new skill"}))
@@ -10,3 +9,10 @@ class EducationForm(forms.Form):
 	education_input_institution = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter institution name"}))
 	education_input_course_title = forms.CharField(max_length = 100, required = False, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter course title"}))
 	education_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details"}))
+
+class ExperienceForm(forms.Form):
+	experience_input_start_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter start year"}))
+	experience_input_end_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter end year"}))
+	experience_input_company = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter company name"}))
+	experience_input_role = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter job title"}))
+	experience_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details"}))
--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from cv.models import KeySkill, Education, Experience
-from .forms import KeySkillForm, EducationForm
+from .forms import KeySkillForm, EducationForm, ExperienceForm
 
 # Create your views here.
 
@@ -8,6 +8,7 @@ def cv(request):
 	if request.method == "POST":
 		new_skill_form = KeySkillForm(request.POST)
 		new_education_form = EducationForm(request.POST)
+		new_experience_form = ExperienceForm(request.POST)
 		if new_skill_form.is_valid():
 			new_skill = KeySkill()
 			new_skill.text = request.POST["skills_input_text"]
@@ -22,7 +23,7 @@ def cv(request):
 			new_education.text = request.POST["education_input_text"]
 			new_education.save()
 			return redirect("/cv/")
-		else:
+		elif new_experience_form.is_valid():
 			new_experience = Experience()
 			new_experience.start_year = request.POST["experience_input_start_year"]
 			new_experience.end_year = request.POST["experience_input_end_year"]
@@ -37,6 +38,7 @@ def cv(request):
 	education_items = Education.objects.all().order_by("-start_year")
 	education_form = EducationForm()
 	experience_items = Experience.objects.all().order_by("-start_year")
+	experience_form = ExperienceForm()
 	return render(request, "cv.html", {"skills": skills, "skills_form": skills_form,
 										"education_items": education_items, "education_form": education_form,
-										"experience_items": experience_items})
+										"experience_items": experience_items, "experience_form": experience_form})

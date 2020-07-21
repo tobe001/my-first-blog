e3f688af569527075ea700a4854ec62972390ced
tobe001 <tgb811@student.bham.ac.uk>
2020-07-21 11:33:46 +0100

    Customised page visually by changing dimensions of text boxes

--- cv/forms.py
+++ cv/forms.py
@@ -8,27 +8,32 @@ class EducationForm(forms.Form):
 	education_input_end_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter end year"}))
 	education_input_institution = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter institution name"}))
 	education_input_course_title = forms.CharField(max_length = 100, required = False, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter course title"}))
-	education_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details"}))
+	education_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details",
+																						"rows": 4, "cols": 75}))
 
 class ExperienceForm(forms.Form):
 	experience_input_start_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter start year"}))
 	experience_input_end_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter end year"}))
 	experience_input_company = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter company name"}))
 	experience_input_role = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter job title"}))
-	experience_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details"}))
+	experience_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details",
+																							"rows": 4, "cols": 75}))
 
 class VolunteeringForm(forms.Form):
 	volunteering_input_start_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter start year"}))
 	volunteering_input_end_year = forms.IntegerField(label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter end year"}))
 	volunteering_input_company = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter company/organisation name"}))
 	volunteering_input_role = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter role title"}))
-	volunteering_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details"}))
+	volunteering_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter further details",
+																							"rows": 4, "cols": 75}))
 
 class ProjectForm(forms.Form):
-	projects_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter details of a new project or achievement"}))
+	projects_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter details of a new project or achievement",
+																						"rows": 4, "cols": 75}))
 
 class HobbyForm(forms.Form):
-	hobbies_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter details of a new activity, skill or hobby"}))
+	hobbies_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter details of a new activity, skill or hobby",
+																						"rows": 4, "cols": 75}))
 
 class ReferenceForm(forms.Form):
 	references_input_name = forms.CharField(max_length = 100, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter name of reference"}))
@@ -37,4 +42,5 @@ class ReferenceForm(forms.Form):
 	references_input_email = forms.CharField(max_length = 100, required = False, label = "", widget = forms.TextInput(attrs = {"placeholder": "Enter email address"}))
 
 class ProfileForm(forms.Form):
-	profile_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter a new profile (will replace existing one)"}))
+	profile_input_text = forms.CharField(label = "", widget = forms.Textarea(attrs = {"placeholder": "Enter a new profile (will replace existing one)",
+																						"rows": 4, "cols": 75}))
